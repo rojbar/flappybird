b37e2ba16fdca31facaec2c3a8e40a8b3a044883
rojbar <55609284+rojbar@users.noreply.github.com>
2022-12-20 08:24:38 -0500

    Added player animation logic and physics

--- src/Escena.ts
+++ src/Escena.ts
@@ -5,14 +5,26 @@ export default class Escena extends Phaser.Scene {
 		super('hello-world')
 	}
 
+	player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+
 	preload() {
 		this.resize();
 		window.addEventListener(`resize`, this.resize, false);
 		this.load.image(`fondo`,`img/espacio.jpg`);
+		this.load.spritesheet(`heroe`,`img/herore.png`,{ frameWidth: 50, frameHeight: 50});
 	}
 
 	create() {
 		this.add.sprite(480,320,`fondo`);
+		
+		this.player = this.physics.add.sprite(50,100, `heroe`);
+		this.anims.create({
+			key: `volar`,
+			frames: this.anims.generateFrameNumbers(`heroe`, {start: 0, end: 1}),
+			frameRate: 7,
+			repeat: -1,
+		});
+		this.player.play(`volar`)
 	}
 
 	resize(){
